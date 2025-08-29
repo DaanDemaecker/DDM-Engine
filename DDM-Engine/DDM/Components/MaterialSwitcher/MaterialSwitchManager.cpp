58aa13aa1aaffd0f294dda241c4806c7458a13bf
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-08-29 12:05:40 +0200

    Added commentary to MaterisalSwitchManager class

--- DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitchManager.cpp
+++ DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitchManager.cpp
@@ -10,6 +10,7 @@
 
 DDM::MaterialSwitchManager::MaterialSwitchManager()
 {
+	// Enable ImGui for this component
 	SetShowImGui(true);
 }
 
@@ -20,16 +21,25 @@ DDM::MaterialSwitchManager::~MaterialSwitchManager()
 
 void DDM::MaterialSwitchManager::RegisterKey(std::string& key)
 {
+	// Check if key is already registered, return if it is
+	if (std::find(m_AvailableKeys.begin(), m_AvailableKeys.end(), key) != m_AvailableKeys.end())
+	{
+		return;
+	}
+	
+	// Register key
 	m_AvailableKeys.push_back(key);
 }
 
 void DDM::MaterialSwitchManager::RegisterKey(std::string&& key)
 {
+	// Propagate to lvalue version
 	RegisterKey(key);
 }
 
 void DDM::MaterialSwitchManager::SetCurrentKey(std::string& key)
 {
+	// Find key in available keys
 	for (int i = 0; i < m_AvailableKeys.size(); ++i)
 	{
 		if (m_AvailableKeys[i].compare(key) == 0)
@@ -39,22 +49,32 @@ void DDM::MaterialSwitchManager::SetCurrentKey(std::string& key)
 		}
 	}
 
+	// Switch material
 	SwitchMaterial(m_CurrentKey);
 }
 
 void DDM::MaterialSwitchManager::SetCurrentKey(std::string&& key)
 {
+	// Propagate to lvalue version
 	SetCurrentKey(key);
 }
 
 void DDM::MaterialSwitchManager::RegisterMaterialSwitcher(std::shared_ptr<MaterialSwitcher> pMaterialSwitcher)
 {
+	// Check if material switcher is already registered, return if it is
+	if (std::find(m_pMaterialSwitchers.begin(), m_pMaterialSwitchers.end(), pMaterialSwitcher) != m_pMaterialSwitchers.end())
+	{
+		return;
+	}
+
+	// Register material switcher
 	m_pMaterialSwitchers.push_back(pMaterialSwitcher);
 }
 
 
 void DDM::MaterialSwitchManager::OnGUI()
 {
+	// Create a list of c-strings from the available keys
 	auto cStringList = std::vector<const char*>();
 
 	cStringList.reserve(m_AvailableKeys.size());
@@ -64,17 +84,19 @@ void DDM::MaterialSwitchManager::OnGUI()
 		cStringList.emplace_back(key.c_str());
 	}
 
+	// Store previous key to check if it changed
 	int prevKey = m_CurrentKey;
 
 	ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_Framed;
 
+	// Create a tree node for the material switcher
 	if (ImGui::TreeNodeEx("Material switcher", flags))
 	{
 		ImGui::ListBox(m_Label.c_str(), &m_CurrentKey, cStringList.data(), static_cast<int>(cStringList.size()));
 		ImGui::TreePop();
 	}
 
-
+	// If key changed, switch material
 	if (prevKey != m_CurrentKey)
 	{
 		SwitchMaterial(m_CurrentKey);
@@ -83,6 +105,7 @@ void DDM::MaterialSwitchManager::OnGUI()
 
 void DDM::MaterialSwitchManager::SwitchMaterial(int key)
 {
+	// Set material of all registered material switchers
 	for (auto& switcher : m_pMaterialSwitchers)
 	{
 		switcher->SetMaterial(m_AvailableKeys[m_CurrentKey]);
--- DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitchManager.h
+++ DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitchManager.h
@@ -15,35 +15,81 @@
 
 namespace DDM
 {
+	// Class forward declarations
 	class MaterialSwitcher;
 
 	class MaterialSwitchManager final : public Component
 	{
 	public:
+		/// <summary>
+		/// Constructor
+		/// </summary>
 		MaterialSwitchManager();
 		
+		/// <summary>
+		/// Destructor
+		/// </summary>
 		~MaterialSwitchManager();
 
+		// Delete copy and move operations
+		MaterialSwitchManager(const MaterialSwitchManager&) = delete;
+		MaterialSwitchManager(MaterialSwitchManager&&) = delete;
+
+		MaterialSwitchManager& operator=(const MaterialSwitchManager&) = delete;
+		MaterialSwitchManager& operator=(MaterialSwitchManager&&) = delete;
+
+		/// <summary>
+		/// Register a new key
+		/// </summary>
+		/// <param name="key: ">name of the key</param>
 		void RegisterKey(std::string& key);
 
+		/// <summary>
+		/// Register a new key
+		/// </summary>
+		/// <param name="key: ">name of the key</param>
 		void RegisterKey(std::string&& key);
 
+		/// <summary>
+		/// Set current key
+		/// </summary>
+		/// <param name="key: ">name of the key</param>
 		void SetCurrentKey(std::string& key);
 
+		/// <summary>
+		/// Set current key
+		/// </summary>
+		/// <param name="key: ">name of the key</param>
 		void SetCurrentKey(std::string&& key);
 
+		/// <summary>
+		/// Register a new material switcher component
+		/// </summary>
+		/// <param name="pMaterialSwitcher: ">pointer to the material switcher component</param>
 		void RegisterMaterialSwitcher(std::shared_ptr<MaterialSwitcher> pMaterialSwitcher);
-
+		
+		/// <summary>
+		/// OnGui function
+		/// </summary>
 		virtual void OnGUI() override;
 
 	private:
+		// Label for the ImGui combo box
 		const std::string m_Label{"Set material"};
 
+		// Current key index
 		int m_CurrentKey{};
+
+		// List of available keys
 		std::vector<std::string> m_AvailableKeys{};
 
+		// List of registered material switchers
 		std::vector<std::shared_ptr<MaterialSwitcher>> m_pMaterialSwitchers{};
 
+		/// <summary>
+		/// Switch the current material
+		/// </summary>
+		/// <param name="key: ">index of the key</param>
 		void SwitchMaterial(int key);
 	};
 }
