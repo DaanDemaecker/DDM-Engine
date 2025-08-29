047254c05d340cf2a5539c534edfc2d36020bf5d
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-08-29 10:03:36 +0200

    Added commentary to MaterialSwitcher class

--- DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitcher.cpp
+++ DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitcher.cpp
@@ -18,22 +18,38 @@ DDM::MaterialSwitcher::~MaterialSwitcher()
 
 void DDM::MaterialSwitcher::RegisterMaterial(std::string& key, std::shared_ptr<Material> pMaterial)
 {
+	// Add material to map
 	m_pRegisteredMaterials[key] = pMaterial;
 }
 
 void DDM::MaterialSwitcher::RegisterMaterial(std::string&& key, std::shared_ptr<Material> pMaterial)
 {
+	// Propagate to lvalue version
 	RegisterMaterial(key, pMaterial);
 }
 
 void DDM::MaterialSwitcher::SetMaterial(std::string key)
 {
+	// Check if material with given key exists
 	if (m_pRegisteredMaterials.contains(key))
 	{
+		// If mesh renderer is not yet set, try to get it
 		if (m_pMeshRenderer == nullptr)
 		{
 			m_pMeshRenderer = GetComponent<DDM::MeshRenderComponent>();
+			// If still null, print error and return
+			if (m_pMeshRenderer == nullptr)
+			{
+				std::cerr << "MaterialSwitcher: No MeshRenderComponent found on GameObject " << GetOwner()->GetName() << std::endl;
+				return;
+			}
 		}
+		// Set the material on the mesh renderer
 		m_pMeshRenderer->SetMaterial(m_pRegisteredMaterials[key]);
+	}
+	else
+	{
+		// Print error if no material with given key exists
+		std::cerr << "MaterialSwitcher: No material found with key " << key << " on GameObject " << GetOwner()->GetName() << std::endl;
 	}
 }
--- DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitcher.h
+++ DDM-Engine/DDM/Components/MaterialSwitcher/MaterialSwitcher.h
@@ -15,25 +15,48 @@
 
 namespace DDM
 {
+	// Class forward declarations
 	class MeshRenderComponent;
 	class Material;
 
 	class MaterialSwitcher final : public Component
 	{
 	public:
+		/// <summary>
+		/// Default constructor
+		/// </summary>
 		MaterialSwitcher();
 
+		/// <summary>
+		/// Destructor
+		/// </summary>
 		~MaterialSwitcher();
 
+		/// <summary>
+		/// Register a new material with a key
+		/// </summary>
+		/// <param name="key: ">key associated with the material</param>
+		/// <param name="pMaterial: ">pointer to the material</param>
 		void RegisterMaterial(std::string& key, std::shared_ptr<Material> pMaterial);
 
+		/// <summary>
+		/// Register a new material with a key
+		/// </summary>
+		/// <param name="key: ">key associated with the material</param>
+		/// <param name="pMaterial: ">pointer to the material</param>
 		void RegisterMaterial(std::string&& key, std::shared_ptr<Material> pMaterial);
 
+		/// <summary>
+		/// Set current material by key
+		/// </summary>
+		/// <param name="key: ">key associated with the material</param>
 		void SetMaterial(std::string key);
 
 	private:
+		// Pointer to the mesh renderer component
 		std::shared_ptr<MeshRenderComponent> m_pMeshRenderer{ nullptr };
 
+		// Map of registered materials
 		std::map<std::string, std::shared_ptr<Material>> m_pRegisteredMaterials{};
 	};
 }
