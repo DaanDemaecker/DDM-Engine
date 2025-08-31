e9b57bbe4a2bd25b9496ed15935fd883bbbda786
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-08-31 17:16:26 +0200

    Added commentary to transform component

--- DDM-Engine/DDM/Components/Transform.cpp
+++ DDM-Engine/DDM/Components/Transform.cpp
@@ -1,24 +1,35 @@
-#include "Transform.h"
-#include "../Includes/GLFWIncludes.h"
-#include "../Includes/ImGuiIncludes.h"
+// Transform.cpp
 
+// Header include
+#include "Transform.h"
+
+// File includes
+#include "Includes/GLFWIncludes.h"
+#include "Includes/ImGuiIncludes.h"
+
+// Standard library includes
 #include <math.h>
 
 void DDM::Transform::OnGUI()
 {
 	ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_Framed;
 
+	// Start a tree node
 	if (ImGui::TreeNodeEx("Transform", flags))
 	{
-
+		// Start a tree node for location
 		if (ImGui::TreeNodeEx("Location", flags))
 		{
+			// Add sliders for all directions
 			ImGui::SliderFloat("x", &m_LocalPosition.x, -10, 10);
 			ImGui::SliderFloat("y", &m_LocalPosition.y, -10, 10);
 			ImGui::SliderFloat("z", &m_LocalPosition.z, -10, 10);
+
+			// End tree node
 			ImGui::TreePop();
 		}
 
+		// Start tree node for rotation
 		if (ImGui::TreeNodeEx("Rotation", flags))
 		{
 			// Convert quaternion to Euler angles in degrees
@@ -32,19 +43,27 @@ void DDM::Transform::OnGUI()
 			// Update quaternion with new Euler angles
 			m_LocalRotation = glm::quat(rotation);
 
+			// End tree node
 			ImGui::TreePop();
 		}
 
+		// Start a tree node for scale
 		if (ImGui::TreeNodeEx("Scale", flags))
 		{
+			// Add slider for each direction
 			ImGui::SliderFloat("x", &m_LocalScale.x, 0.001f, 2.0f);
 			ImGui::SliderFloat("y", &m_LocalScale.y, 0.001f, 2.0f);
 			ImGui::SliderFloat("z", &m_LocalScale.z, 0.001f, 2.0f);
+
+			// End tree node
 			ImGui::TreePop();
 		}
+
+		// End tree node
 		ImGui::TreePop();
 	}
 
+	// Set the dirty flag
 	SetPositionDirtyFlag();
 	SetRotationDirtyFlag();
 	SetScaleDirtyFlag();
@@ -52,24 +71,41 @@ void DDM::Transform::OnGUI()
 
 void DDM::Transform::SetLocalPosition(float x, float y, float z)
 {
+	// Propagate to overloaded function
 	SetLocalPosition(glm::vec3{ x, y, z });
 }
 
 void DDM::Transform::SetLocalPosition(const glm::vec3& pos)
 {
+	// Set new local position
 	m_LocalPosition = pos;
 
+	// Set dirty flag
 	SetPositionDirtyFlag();
+}
+
+void DDM::Transform::SetLocalPosition(const glm::vec3&& pos)
+{
+	// Propagate to lvalue overloaded function
+	SetLocalPosition(pos);
 }
 
 void DDM::Transform::Translate(float x, float y, float z)
 {
+	// Propagate to overloaded function
 	Translate(glm::vec3{ x, y, z });
 }
 
 void DDM::Transform::Translate(const glm::vec3& dir)
 {
+	// Set new local position
 	SetLocalPosition(m_LocalPosition + dir);
+}
+
+void DDM::Transform::Translate(const glm::vec3&& dir)
+{
+	// Propagate to lvalue overloaded function
+	Translate(dir);
 }
 
 glm::vec3 DDM::Transform::GetWorldPosition()
@@ -94,92 +130,150 @@ glm::vec3 DDM::Transform::GetWorldPosition()
 
 void DDM::Transform::SetWorldPosition(float x, float y, float z)
 {
+	// Propagate to overloaded function
 	SetWorldPosition(glm::vec3{ x, y, z });
 }
 
 void DDM::Transform::SetWorldPosition(const glm::vec3& pos)
 {
+	// Set new local position
 	SetLocalPosition(pos - (GetWorldPosition() - m_LocalPosition));
+}
+
+void DDM::Transform::SetWorldPosition(const glm::vec3&& pos)
+{
+	// Propagate to lvalue overloaded function
+	SetWorldPosition(pos);
 }
 
 void DDM::Transform::SetPositionDirtyFlag() const
 {
+	// Loop through all children
 	for (auto& pChild : GetOwner()->GetChildren())
 	{
+		// Extract transform and set dirty flag
 		auto transform = pChild->GetTransform();
 		transform->m_PositionDF = true;
+
+		// Propagate dirty flag through all children
 		transform->SetPositionDirtyFlag();
 	}
 }
 
+const glm::vec3 DDM::Transform::GetLocalRotationEuler() const
+{
+	// Convert rotation to euler angles and return
+	return glm::eulerAngles(m_LocalRotation);
+}
+
 void DDM::Transform::SetLocalRotation(float x, float y, float z)
 {
+	// Propagate to overloaded function
 	SetLocalRotation(glm::vec3{ x, y ,z });
 }
 
 void DDM::Transform::SetLocalRotation(const glm::vec3& rot)
 {
-	m_LocalRotation = glm::quat(rot);
-
-	SetRotationDirtyFlag();
+	// Convert to quaternion and propagate to overloaded function
+	SetLocalRotation(glm::quat(rot));
 }
 
 void DDM::Transform::SetLocalRotation(const glm::vec3&& rot)
 {
+	// Propagate to lvalue overloaded function
 	SetLocalRotation(rot);
 }
 
 void DDM::Transform::SetLocalRotation(const glm::quat& rot)
 {
+	// Set local rotation
 	m_LocalRotation = rot;
 
+	// Set dirty flag for rotation
 	SetRotationDirtyFlag();
 }
 
 void DDM::Transform::SetLocalRotation(const glm::quat&& rot)
 {
+	// Propagate to lvalue overloaded function
 	SetLocalRotation(rot);
 }
 
 void DDM::Transform::Rotate(glm::vec3& axis, float angle)
 {
+	// Rotate local rotation around axiss
 	m_LocalRotation = glm::rotate(m_LocalRotation, angle, axis);
 
+	// Set dirty flag for rotation
 	SetRotationDirtyFlag();
 }
 
 void DDM::Transform::Rotate(glm::vec3&& axis, float angle)
 {
+	// Propagate to lvalue overloaded function
 	Rotate(axis, angle);
+}
+
+void DDM::Transform::RotateDeg(glm::vec3& axis, float angle)
+{
+	// Convert to radians and propagate
+	Rotate(axis, glm::radians(angle));
+}
+
+void DDM::Transform::RotateDeg(glm::vec3&& axis, float angle)
+{
+	// Propagate to lvalue overloaded function
+	RotateDeg(axis, angle);
 }
 
 glm::quat DDM::Transform::GetWorldRotation()
 {
+	// Calculate and return world rotation
 	return GetParentRotation() * m_LocalRotation;
 }
 
 void DDM::Transform::SetWorldRotation(float x, float y, float z)
 {
+	// Propagate to overloaded function
 	SetWorldRotation(glm::vec3{x, y, z});
 }
 
 void DDM::Transform::SetWorldRotation(const glm::vec3& rot)
 {
-	// Calculate the rotation needed to achieve the desired world rotation
+	// Convert to quaternion
 	glm::quat worldRotation = glm::quat(rot);
 
+	// Propagate to overloaded function
+	SetWorldRotation(worldRotation);
+}
+
+void DDM::Transform::SetWorldRotation(const glm::vec3&& rot)
+{
+	// Propagate to lvalue overloaded function
+	SetWorldRotation(rot);
+}
+
+void DDM::Transform::SetWorldRotation(const glm::quat& rot)
+{
 	// Get the parent's world rotation
 	glm::quat parentWorldRotation = GetParentRotation();
 
 	// Adjust the local rotation to achieve the desired world rotation
-	glm::quat localRotation =  worldRotation * glm::inverse(parentWorldRotation);
+	glm::quat localRotation = rot * glm::inverse(parentWorldRotation);
 
 	// Set the local rotation
 	SetLocalRotation(localRotation);
 }
 
+void DDM::Transform::SetWorldRotation(const glm::quat&& rot)
+{
+	// Propagate to lvalue overloaded function
+	SetWorldRotation(rot);
+}
+
 void DDM::Transform::SetRotationDirtyFlag() const
 {
+	// Loop through all children, set dirty flag and propagate
 	for (auto& pChild : GetOwner()->GetChildren())
 	{
 		auto transform = pChild->GetTransform();
@@ -190,14 +284,23 @@ void DDM::Transform::SetRotationDirtyFlag() const
 
 void DDM::Transform::SetLocalScale(float x, float y, float z)
 {
+	// Propagate to overloaded function
 	SetLocalScale(glm::vec3{ x, y, z });
 }
 
 void DDM::Transform::SetLocalScale(const glm::vec3& scale)
 {
+	// Set local scale
 	m_LocalScale = scale;
 
+	// Set dirty flag
 	SetScaleDirtyFlag();
+}
+
+void DDM::Transform::SetLocalScale(const glm::vec3&& scale)
+{
+	// Propagate to lvalue overloaded function
+	SetLocalScale(scale);
 }
 
 glm::vec3 DDM::Transform::GetWorldScale()
@@ -207,16 +310,26 @@ glm::vec3 DDM::Transform::GetWorldScale()
 
 void DDM::Transform::SetWorldScale(float x, float y, float z)
 {
+
+	// Propagate to overloaded function
 	SetWorldScale(glm::vec3(x, y, z));
 }
 
-void DDM::Transform::SetWorldScale(const glm::vec3 scale)
+void DDM::Transform::SetWorldScale(const glm::vec3& scale)
 {
+	// Calculate new local scale and set it
 	SetLocalScale(scale/(GetWorldScale()/m_LocalScale));
+}
+
+void DDM::Transform::SetWorldScale(const glm::vec3&& scale)
+{
+	// Propagate to lvalue overloaded function
+	SetWorldScale(scale);
 }
 
 void DDM::Transform::SetScaleDirtyFlag() const
 {
+	// Loop through all children, set dirty flag and propagate
 	for (auto& pChild : GetOwner()->GetChildren())
 	{
 		auto transform = pChild->GetTransform();
@@ -269,50 +382,59 @@ glm::vec3 DDM::Transform::GetRight()
 	return finalRotatedVector;
 }
 
-glm::vec3& DDM::Transform::GetParentPosition()
+const glm::vec3& DDM::Transform::GetParentPosition()
 {
+	// If no owner or parent, set parent world position to 0,0,0
 	if (GetOwner() == nullptr || GetOwner()->GetParent() == nullptr)
 	{
 		m_ParentWorldPosition = glm::vec3{};
 		m_PositionDF = false;
 	}
+	// If Dirty flag is set, save new parent position
 	else if (m_PositionDF)
 	{
 		m_ParentWorldPosition = GetOwner()->GetParent()->GetTransform()->GetWorldPosition();
 		m_PositionDF = false;
 	}
 
+	// return parent position
 	return m_ParentWorldPosition;
 }
 
-glm::quat& DDM::Transform::GetParentRotation()
+const glm::quat& DDM::Transform::GetParentRotation()
 {
+	// If no owner or parent, set parent world rotation to identity quaternion
 	if (GetOwner() == nullptr || GetOwner()->GetParent() == nullptr)
 	{
 		m_ParentWorldRotation = glm::identity<glm::quat>();;
 		m_RotationDF = false;
 	}
+	// If dirty flag is set, save new parent rotation
 	else if (m_RotationDF)
 	{
 		m_ParentWorldRotation = GetOwner()->GetParent()->GetTransform()->GetWorldRotation();
 		m_RotationDF = false;
 	}
 
+	// Return parent rotation
 	return m_ParentWorldRotation;
 }
 
-glm::vec3& DDM::Transform::GetParentScale()
+const glm::vec3& DDM::Transform::GetParentScale()
 {
+	// If no owner or parent, set parent world scale to 1,1,1
 	if (GetOwner() == nullptr || GetOwner()->GetParent() == nullptr)
 	{
 		m_ParentWorldScale = glm::vec3{ 1, 1, 1 };
 		m_ScaleDF = false;
 	}
+	// If dirty flag is set, save new parent scale
 	else if (m_ScaleDF)
 	{
 		m_ParentWorldScale = GetOwner()->GetParent()->GetTransform()->GetWorldScale();
 		m_ScaleDF = false;
 	}
 
+	// Return parent scale
 	return m_ParentWorldScale;
 }
--- DDM-Engine/DDM/Components/Transform.h
+++ DDM-Engine/DDM/Components/Transform.h
@@ -1,81 +1,385 @@
-#ifndef TransformComponenetIncluded
-#define TransformComponenetIncluded
-#include "../BaseClasses/Component.h"
-#include "../Includes/GLMIncludes.h"
+// Transform.h
+// THis class will represent the transform of the object
+// The transform holds and handles rotation, position and scale
+
+#ifndef _TRANSFORM_
+#define _TRANSFORM_
+
+// Parent include
+#include "BaseClasses/Component.h"
+
+// File includes
+#include "Includes/GLMIncludes.h"
 
 namespace DDM
 {
 	class Transform final : public Component
 	{
 	public:
+		/// <summary>
+		/// Default constructor
+		/// </summary>
 		Transform() = default;
+
+		/// <summary>
+		/// Default destructor
+		/// </summary>
 		~Transform() = default;
 
+		// Rule of five
+		Transform(Transform&) = delete;
+		Transform(Transform&&) = delete;
+
+		Transform& operator=(Transform&) = delete;
+		Transform& operator=(Transform&&) = delete;
+
+		/// <summary>
+		/// OnGUi function
+		/// </summary>
 		virtual void OnGUI() override;
 
-		//Location
+		// -------------------
+		// Location
+		// -------------------
+
+		/// <summary>
+		/// Get the local position of the object
+		/// </summary>
+		/// <returns>Reference to local position</returns>
 		const glm::vec3& GetLocalPosition() const { return m_LocalPosition; }
+
+		/// <summary>
+		/// Set local position
+		/// </summary>
+		/// <param name="x: ">new x value</param>
+		/// <param name="y: ">new y value</param>
+		/// <param name="z: ">new z value</param>
 		void SetLocalPosition(float x, float y, float z);
+
+		/// <summary>
+		/// Set local position
+		/// </summary>
+		/// <param name="pos: ">new local position</param>
 		void SetLocalPosition(const glm::vec3& pos);
 
+		/// <summary>
+		/// Set local position
+		/// </summary>
+		/// <param name="pos: ">new local position</param>
+		void SetLocalPosition(const glm::vec3&& pos);
+
+		/// <summary>
+		/// Translate the object a given amount
+		/// </summary>
+		/// <param name="x: ">translate x value</param>
+		/// <param name="y: ">translate y value</param>
+		/// <param name="z: ">translate z value</param>
 		void Translate(float x, float y, float z);
+
+		/// <summary>
+		/// Translate the object a given amount
+		/// </summary>
+		/// <param name="dir: ">translate value</param>
 		void Translate(const glm::vec3& dir);
 
+		/// <summary>
+		/// Translate the object a given amount
+		/// </summary>
+		/// <param name="dir: ">translate value</param>
+		void Translate(const glm::vec3&& dir);
+
+		/// <summary>
+		/// Get the world position of the object
+		/// </summary>
+		/// <returns>World position by value</returns>
 		glm::vec3 GetWorldPosition();
+
+		/// <summary>
+		/// Set the world position of the object
+		/// </summary>
+		/// <param name="x: ">new x value of worldpos</param>
+		/// <param name="y: ">new y value of worldpos</param>
+		/// <param name="z: ">new z value of worldpos</param>
 		void SetWorldPosition(float x, float y, float z);
+
+		/// <summary>
+		/// Set the world position of the object
+		/// </summary>
+		/// <param name="pos: ">new value of the world position</param>
 		void SetWorldPosition(const glm::vec3& pos);
 
+		/// <summary>
+		/// Set the world position of the object
+		/// </summary>
+		/// <param name="pos: ">new value of the world position</param>
+		void SetWorldPosition(const glm::vec3&& pos);
+
+		/// <summary>
+		/// Set the dirty flag for the parent position
+		/// </summary>
 		void SetPositionDirtyFlag() const;
 
-		//Rotation
+		
+		// -------------------
+		// Rotation
+		// -------------------
+		
+		/// <summary>
+		/// Get the local rotation quaternion
+		/// </summary>
+		/// <returns>Reference to local rotation quaternion</returns>
 		const glm::quat& GetLocalRotation() const { return m_LocalRotation; }
+
+		/// <summary>
+		/// Get local rotation in euler angles
+		/// </summary>
+		/// <returns>Local rotation euler angles</returns>
+		const glm::vec3 GetLocalRotationEuler() const;
+		
+		/// <summary>
+		/// Set local rotation in euler angles
+		/// </summary>
+		/// <param name="x: ">new x value for euler angles</param>
+		/// <param name="y: ">new y value for euler angles</param>
+		/// <param name="z: ">new z value for euler angles</param>
 		void SetLocalRotation(float x, float y, float z);
+
+		/// <summary>
+		/// Set local rotation in euler angles
+		/// </summary>
+		/// <param name="rot: ">new value for euler angles</param>
 		void SetLocalRotation(const glm::vec3& rot);
+
+		/// <summary>
+		/// Set local rotation in euler angles
+		/// </summary>
+		/// <param name="rot: ">new value for euler angles</param>
 		void SetLocalRotation(const glm::vec3&& rot);
+
+		/// <summary>
+		/// Set local rotation as quaternion
+		/// </summary>
+		/// <param name="rot: ">new value for rotation</param>
 		void SetLocalRotation(const glm::quat& rot);
+
+		/// <summary>
+		/// Set local rotation as quaternion
+		/// </summary>
+		/// <param name="rot: ">new value for rotation</param>
 		void SetLocalRotation(const glm::quat&& rot);
 
+		/// <summary>
+		/// Rotate the object over an axis
+		/// </summary>
+		/// <param name="axis: ">the axis over which to rotate</param>
+		/// <param name="angle: ">angle to rotate in radians</param>
 		void Rotate(glm::vec3& axis, float angle);
+
+		/// <summary>
+		/// Rotate the object over an axis
+		/// </summary>
+		/// <param name="axis: ">the axis over which to rotate</param>
+		/// <param name="angle: ">angle to rotate in radians</param>
 		void Rotate(glm::vec3&& axis, float angle);
 
+		/// <summary>
+		/// Rotate the object over an axis
+		/// </summary>
+		/// <param name="axis: ">the axis over which to rotate</param>
+		/// <param name="angle: ">angle to rotate in degrees</param>
+		void RotateDeg(glm::vec3& axis, float angle);
+
+		/// <summary>
+		/// Rotate the object over an axis
+		/// </summary>
+		/// <param name="axis: ">the axis over which to rotate</param>
+		/// <param name="angle: ">angle to rotate in degrees</param>
+		void RotateDeg(glm::vec3&& axis, float angle);
+
+		/// <summary>
+		/// Get world rotation
+		/// </summary>
+		/// <returns>Quaternion signifying world rotation</returns>
 		glm::quat GetWorldRotation();
+
+		/// <summary>
+		/// Set the world rotation in euler angles
+		/// </summary>
+		/// <param name="x: ">new x value of rotation</param>
+		/// <param name="y: ">new y value of rotation</param>
+		/// <param name="z: ">new z value of rotation</param>
 		void SetWorldRotation(float x, float y, float z);
+
+		/// <summary>
+		/// Set the world rotation in euler angles
+		/// </summary>
+		/// <param name="x: ">new value of rotation</param>
 		void SetWorldRotation(const glm::vec3& rot);
 
+		/// <summary>
+		/// Set the world rotation in euler angles
+		/// </summary>
+		/// <param name="x: ">new value of rotation</param>
+		void SetWorldRotation(const glm::vec3&& rot);
+
+		/// <summary>
+		/// Set the world rotation as a quaternion
+		/// </summary>
+		/// <param name="rot">new valu of rotation</param>
+		void SetWorldRotation(const glm::quat& rot);
+
+		/// <summary>
+		/// Set the world rotation as a quaternion
+		/// </summary>
+		/// <param name="rot">new valu of rotation</param>
+		void SetWorldRotation(const glm::quat&& rot);
+
+
+		/// <summary>
+		/// Set the dirty flag for the rotation
+		/// </summary>
 		void SetRotationDirtyFlag() const;
 
-		//Scale
+		// -------------------
+		// Scale
+		// -------------------
+		
+		/// <summary>
+		/// Get local scale
+		/// </summary>
+		/// <returns>reference to local scene</returns>
 		const glm::vec3& GetLocalScale() const { return m_LocalScale; }
+
+		/// <summary>
+		/// Set local scale
+		/// </summary>
+		/// <param name="x: ">new x value for scale</param>
+		/// <param name="y: ">new y value for scale</param>
+		/// <param name="z: ">new z value for scale</param>
 		void SetLocalScale(float x, float y, float z);
+
+		/// <summary>
+		/// Set local scale
+		/// </summary>
+		/// <param name="scale: ">new value for scale</param>
 		void SetLocalScale(const glm::vec3& scale);
 
-		glm::vec3 GetWorldScale();
-		void SetWorldScale(float x, float y, float z);
-		void SetWorldScale(const  glm::vec3 scale);
+		/// <summary>
+		/// Set local scale
+		/// </summary>
+		/// <param name="scale: ">new value for scale</param>
+		void SetLocalScale(const glm::vec3&& scale);
 
+
+		/// <summary>
+		/// Get the world scale
+		/// </summary>
+		/// <returns>World scale</returns>
+		glm::vec3 GetWorldScale();
+
+		/// <summary>
+		/// Set world scale
+		/// </summary>
+		/// <param name="x: ">new x value for scale</param>
+		/// <param name="y: ">new y value for scale</param>
+		/// <param name="z: ">new z value for scale</param>
+		void SetWorldScale(float x, float y, float z);
+
+		/// <summary>
+		/// Set world scale
+		/// </summary>
+		/// <param name="scale: ">new value for scale</param>
+		void SetWorldScale(const glm::vec3& scale);
+
+		/// <summary>
+		/// Set world scale
+		/// </summary>
+		/// <param name="scale: ">new value for scale</param>
+		void SetWorldScale(const glm::vec3&& scale);
+
+		/// <summary>
+		/// Set dirty flag for scale
+		/// </summary>
 		void SetScaleDirtyFlag() const;
 
+
+		/// <summary>
+		/// Get the forward direction of the object
+		/// </summary>
+		/// <returns>Forward vector</returns>
 		glm::vec3 GetForward();
+
+		/// <summary>
+		/// Get the up direction of the object
+		/// </summary>
+		/// <returns>Up vector</returns>
 		glm::vec3 GetUp();
+
+		/// <summary>
+		/// Get right direction of the object
+		/// </summary>
+		/// <returns>Right vector</returns>
 		glm::vec3 GetRight();
 
 
 	private:
+
+		// -------------------
+		// Position
+		// -------------------
+		
+		// Position relative to parent
 		glm::vec3 m_LocalPosition{};
+
+		// World position of parent
 		glm::vec3 m_ParentWorldPosition{};
+
+		// Position dirty flag
 		bool m_PositionDF{ true };
 
+		// -------------------
+		// Rotation
+		// -------------------
+
+		// Rotation relative to parent
 		glm::quat m_LocalRotation{ glm::identity<glm::quat>() };
+
+		// World rotation of parent
 		glm::quat m_ParentWorldRotation{};
+
+		// Rotation dirty flag
 		bool m_RotationDF{ true };
 
+		// -------------------
+		// Scale
+		// -------------------
+
+		// Scale relative to parent
 		glm::vec3 m_LocalScale{1, 1, 1};
+
+		// World scale of parent
 		glm::vec3 m_ParentWorldScale{};
+
+		// Scale dirty flag
 		bool m_ScaleDF{ true };
 
-		glm::vec3& GetParentPosition();
-		glm::quat& GetParentRotation();
-		glm::vec3& GetParentScale();
+		/// <summary>
+		/// Get parent position
+		/// </summary>
+		/// <returns>Reference to parent position</returns>
+		const glm::vec3& GetParentPosition();
+
+		/// <summary>
+		/// Get parent rotation
+		/// </summary>
+		/// <returns>Reference to parent rotation</returns>
+		const glm::quat& GetParentRotation();
+
+		/// <summary>
+		/// Get parent scale
+		/// </summary>
+		/// <returns>Reference to parent rotation</returns>
+		const glm::vec3& GetParentScale();
 	};
 }
-#endif // !TransformComponenetIncluded
+#endif // !_TRANSFORM_
