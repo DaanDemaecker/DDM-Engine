8b0e3e36702f7c4aa8a082c739c9c6e2ad49d48a
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-09-13 21:09:00 +0200

    Added commentary to DDMEngine class

--- DDM-Engine/DDM/Engine/DDMEngine.cpp
+++ DDM-Engine/DDM/Engine/DDMEngine.cpp
@@ -33,83 +33,112 @@ DDM::DDMEngine::~DDMEngine()
 
 void DDM::DDMEngine::Run(const std::function<void()>& load)
 {
+	// If not initialized, return
 	if (!m_Initialized)
 	{
 		std::cout << "Engine not initialized \n";
 		return;
 	}
 
+	// Call load function
 	load();
 
-	auto& renderer{ VulkanObject::GetInstance() };
+	// Get necesarry objects for later use
+	auto& vulkanObject{ VulkanObject::GetInstance() };
 	auto& sceneManager{ SceneManager::GetInstance() };
 	auto& time{ TimeManager::GetInstance() };
 	auto& window{ Window::GetInstance() };
 	auto& input{ InputManager::GetInstance() };
 
-
+	// Indicates wether the update loop should continue
 	bool doContinue = true;
+
+	// The timestamp of the previous frame
 	auto lastTime = std::chrono::high_resolution_clock::now();
 
+	// Time lag for physics update
 	float lag = 0.0f;
+
+	// Fixed step for the physics update
 	constexpr float fixedTimeStep = 0.02f;
 
+	// Set the fixed time
 	time.SetFixedTime(fixedTimeStep);
 
+	// Indicates wether desired framerate should be followed
 	constexpr bool followDesiredFramerate = false;
+
+	// Desired framerate
 	constexpr float desiredFrameRate = 144.f;
+
+	// Desired duration of each frame
 	constexpr float desiredFrameDuration = 1000.f / desiredFrameRate;
 
 
 	while (doContinue)
 	{
+		// Get timestamp of current frame and calculate delta time
 		const auto currentTime = std::chrono::high_resolution_clock::now();
 		const float deltaTime = std::chrono::duration<float>(currentTime - lastTime).count();
 
+		// Set the deltatime
 		time.SetDeltaTime(deltaTime);
 
+		// Process the input
 		input.ProcessInput();
 
-		//std::cout << time.GetFps() << std::endl;
-
+		// Set last time to current time for next frame
 		lastTime = currentTime;
+
+		// Add deltatime to lag
 		lag += deltaTime;
 
+		// Check if window should close
 		doContinue = !glfwWindowShouldClose(window.GetWindowStruct().pWindow);
 
+		// Poll glfw events
 		glfwPollEvents();
 
+		// Start the frame, used to initialize objects
 		sceneManager.StartFrame();
 
+		// Call early update
 		sceneManager.EarlyUpdate();
 
+		// Handle the fixed update
 		while (lag >= fixedTimeStep)
 		{
 			sceneManager.FixedUpdate();
 			lag -= fixedTimeStep;
 		}
 
+		// Call update
 		sceneManager.Update();
 
+		// Call late update
 		sceneManager.LateUpdate();
 
+		// Call post update, used
 		sceneManager.PostUpdate();
 
-		renderer.Render();
+		// Render scene
+		vulkanObject.Render();
 
-		if (input.GetKeyUp(GLFW_KEY_SPACE))
-		{
-			std::cout << "yes" << std::endl;
-		}
-
+		// Calculate the duration of the frame so far
 		const auto frameDuration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - currentTime);
+		
+		// Calculate how long the thread should sleep to keep desired frame duration
 		const auto sleepTime = std::chrono::milliseconds(static_cast<int>(desiredFrameDuration)) - frameDuration;
 
+		// Only sleep if desired framerate should be followed
+		// and sleeptime  is over 0
 		if (followDesiredFramerate && sleepTime > std::chrono::milliseconds(0))
 		{
+			// Sleep for the calculated amount of time
 			std::this_thread::sleep_for(sleepTime);
 		}
 	}
 
+	// Clean up all objects
 	sceneManager.EndProgram();
 }
--- DDM-Engine/DDM/Engine/DDMEngine.h
+++ DDM-Engine/DDM/Engine/DDMEngine.h
@@ -1,5 +1,8 @@
-#ifndef D3DEngineIncluded
-#define D3DEngineIncluded
+// DDMEngine.h
+// This class will serve as the core engine, managing the update loop and initialization of certain objects
+
+#ifndef _DDM_DDM_ENGINE_
+#define _DDM_DDM_ENGINE_
 
 // File includes
 #include "Includes/GLFWIncludes.h"
@@ -15,20 +18,37 @@ namespace DDM
 	class DDMEngine
 	{
 	public:
+		/// <summary>
+		/// Constructor
+		/// </summary>
 		DDMEngine();
+		
+		/// <summary>
+		/// Destructor
+		/// </summary>
 		~DDMEngine();
 
+		/// <summary>
+		/// Initalizes the engine
+		/// </summary>
+		/// <typeparam name="T: ">Type of renderer to use</typeparam>
 		template <class T>
 		void Init();
 
+		/// <summary>
+		/// Runs the engine
+		/// </summary>
+		/// <param name="load: ">Reference to the sceneloader function</param>
 		void Run(const std::function<void()>& load);
 
+		// Rule of 5
 		DDMEngine(const DDMEngine& other) = delete;
 		DDMEngine(DDMEngine&& other) = delete;
 		DDMEngine& operator=(const DDMEngine& other) = delete;
 		DDMEngine& operator=(DDMEngine&& other) = delete;
 	
 	private:
+		// Indicates wether engine is initialized
 		bool m_Initialized = false;
 	};
 
@@ -38,13 +58,14 @@ namespace DDM
 		// Create the window with the given width and height
 		DDM::Window::GetInstance();
 
+		// Get a reference to the vulkan object and initialize
 		auto& vulkanObject = VulkanObject::GetInstance();
-
 		vulkanObject.Init<T>();
 
+		// Indicate that engine is initialized
 		m_Initialized = true;
 	}
 
 }
 
-#endif // !D3DEngineIncluded
+#endif // !_DDM_DDM_ENGINE_
